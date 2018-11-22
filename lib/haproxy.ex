5447d3b1bd6c9f4795ed0d284e2d384ebc9cc346
seika <43219386+syfgkjasdkn@users.noreply.github.com>
2018-11-22 13:51:08 +0000

    fix typo

--- lib/haproxy.ex
+++ lib/haproxy.ex
@@ -8,7 +8,7 @@ defmodule HAProxy do
 
   @spec query(:gen_tcp.socket(), String.t()) :: {:ok, String.t()} | {:error, any}
   @spec query(:gen_tcp.socket(), String.t(), pos_integer) :: {:ok, String.t()} | {:error, any}
-  def query(socket, query, timeout \\ 1_000) do
+  def query(sock, query, timeout \\ 1_000) do
     :ok = :gen_tcp.send(sock, <<"#{query}\n\r", 0>>)
     :gen_tcp.recv(sock, 0, timeout)
   end
